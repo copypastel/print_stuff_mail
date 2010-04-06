a229caf5f905dcd859e79638299f7f63e3d7a4ff
ecin <ecin@copypastel.com>
2010-04-05 22:39:18 -0400

    Have more than two test files, that means a test_helper is needed to soak up any wetness.

---
+++ test/test_helper.rb
@@ -0,0 +1,5 @@
+$LOAD_PATH.unshift File.expand_path( File.dirname(__FILE__) + '/../lib')
+
+require File.expand_path( File.dirname(__FILE__) + '/fake_service')
+require 'shoulda'
+require 'artifice'
